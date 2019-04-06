598cf493b2777cc009718e85f08297f5fdb75269
maaz <maazfavourite93@gmail.com>
2019-04-06 22:11:20 +0530

    added responsive nav bar which can work without bootstrap

--- src/components/navigaion.jsx
+++ src/components/navigaion.jsx
@@ -3,57 +3,49 @@ import "bootstrap/dist/css/bootstrap.css";
 import { Link } from "react-router-dom";
 import imdb from "../logo/imdb.png";
 
+import { Navbar, Nav } from "react-bootstrap";
 class Navigation extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon" />
-          </button>
+        <Navbar bg="light" expand="lg">
           <img
             src={imdb}
             alt="Italian Trulli"
             style={{ width: 80, height: 50 }}
           />
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <Link className="nav-link" to="../LoginForm">
-                  Login <span className="sr-only">(current)</span>
+          <Navbar.Brand href="#home">Movies-Project</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link>
+                <Link to="../LoginForm" style={{ color: "#000000" }}>
+                  Login
                 </Link>
-              </li>
-              <li className="nav-item active">
-                <Link className="nav-link" to="../RegisterForm">
-                  Register <span className="sr-only">(current)</span>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="../RegisterForm" style={{ color: "#000000" }}>
+                  Register
                 </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="../Movies">
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="../Movies" style={{ color: "#000000" }}>
                   Movies
                 </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="../Customers">
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="../Customers" style={{ color: "#000000" }}>
                   Customers
                 </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="../Rentals">
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="../Rentals" style={{ color: "#000000" }}>
                   Rentals
                 </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </React.Fragment>
     );
   }
